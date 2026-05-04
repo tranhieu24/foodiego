aea4680c2784483e79b3fd289215bb9be2872d17
Tran Hieu <23710931.hieu@student.iuh.edu.vn>
2026-05-04 12:44:19 +0700

    fix: remove double semicolon in orderRoutes

--- server/src/routes/orderRoutes.js
+++ server/src/routes/orderRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getMyOrders, updateOrderToPaid  } from '../controllers/orderController.js';
-import { protect  } from '../middleware/auth.js';
+import { addOrderItems, getMyOrders, updateOrderToPaid } from '../controllers/orderController.js';
+import { protect } from '../middleware/auth.js';
 
 router.post('/', protect, addOrderItems);
 router.get('/user/:id', protect, getMyOrders);
 router.put('/:id/pay', protect, updateOrderToPaid);
 
-export default router;;
+export default router;
